96dc2f0fe3e4d478001ebcb883fcc08f6ecd1818
YildirimCat <yldrmcat68@hotmail.com>
2022-01-11 21:35:48 +0300

    3.gün sonu

---
+++ Methods/ChartManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class ChartManager
+    {
+
+        //Best Practice
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.Name + " sepete eklendi.");
+        }
+
+        //Bad usage
+        public void Add2(string productName,string productExp,int price)
+        {
+            Console.WriteLine(productName + " sepete eklendi.");
+        }
+    }
+}
