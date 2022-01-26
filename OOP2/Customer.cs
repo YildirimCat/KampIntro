a8920ec43b763ea3313ee3a7b8038b4eb4f32e3e
YildirimCat <yldrmcat68@hotmail.com>
2022-01-26 15:15:03 +0300

    5.Hafta Sonu OOP2

---
+++ OOP2/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{   
+    
+    //Base Class
+    internal class Customer
+    {
+
+        public int Id { get; set; }
+        public string CustomerNo { get; set; }
+   
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Customer customer1 = new Customer();
+            //customer1.Id = 1;
+            //customer1.Name = "Tom";
+            //customer1.Surname = "Bombadil";
+            //customer1.IdentificationNo = "123143141";
+            
+            
+            //Tom Bombadil gerçek bir kişi ve bir şirketi veya vergi numarası yok!
+            //customer1.CompanyName = "????";
+            //customer1.TaxNo = "??";
+
+
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.Name = "Stannis";
+            customer1.Surname = "Baratheon";
+            customer1.CustomerNo = "12345";
+            customer1.IdentificationNo = "31314123123";
+
+
+            CorporateCustomer customer2 = new CorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNo = "12346";
+            customer2.TaxNo = "3123412313";
+            customer2.CompanyName = "Microsoft";
+
+            //Base class bir referans tutucudur.
+            Customer customer3 = new CorporateCustomer();
+            Customer customer4 = new IndividualCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.Add(customer4);
+        }
+
+    }
+}
+
+
